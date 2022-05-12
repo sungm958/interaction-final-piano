92792681ed25258dd348051081a288b15bc0b823
sungm958 <99030811+sungm958@users.noreply.github.com>
2022-05-12 19:14:21 -0400

    Add files via upload

---
+++ image.js
@@ -0,0 +1,17 @@
+var myImage = document.getElementById('mainImage');
+image1.onclick = function(){
+	myImage.setAttribute("src", "jinglebell.jpeg");
+}
+image2.onclick = function(){
+	myImage.setAttribute("src", "twinkle.jpeg");
+}
+image3.onclick = function(){
+	myImage.setAttribute("src", "iloveu.jpeg");
+}
+image4.onclick = function(){
+	myImage.setAttribute("src", "hbd.jpeg");
+}
+image5.onclick = function(){
+	myImage.setAttribute("src", "row.jpeg");
+}
+
